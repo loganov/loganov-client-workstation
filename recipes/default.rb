a71959d6c035d054076d70c9711b4c913f828fba
Gregory Weaver <gregory.weaver@gmail.com>
2014-08-02 12:38:22 -0700

    Initial.

---
+++ recipes/default.rb
@@ -0,0 +1,12 @@
+#http://media.steampowered.com/client/installer/steam.deb
+
+#[steam_fedora19]
+#name=Steam RPM packages (and dependencies) for Fedora
+#baseurl=http://negativo17.org/repos/steam/fedora-19/x86_64/
+#enabled=0
+#skip_if_unavailable=1
+#gpgcheck=0
+
+#yum install http://download1.rpmfusion.org/free/fedora/releases/19/Everything/i386/os/libtxc_dxtn-1.0.0-3.fc19.i686.rpm
+
+#yum --enablerepo=steam_fedora19 install steam
