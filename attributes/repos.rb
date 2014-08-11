5c17d603a61006b18aaa30d37d7f3af837793135
Gregory Weaver <gregory.weaver@gmail.com>
2014-08-11 15:14:47 -0700

    Adding supporting files.

---
+++ attributes/repos.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: loganov-client-workstation
+# Attribute:: repos
+#
+# Copyright 2014, Loganov Industries, LLC
+#
+
+#default['loganov']['client-workstation']['repo']['elrepo']['url'] =  
+
+#default['loganov']['client-workstation']['repo']['rpmforge']['url'] =  
+
+#default['loganov']['client-workstation']['repo']['nux_desktop']['url'] =  
