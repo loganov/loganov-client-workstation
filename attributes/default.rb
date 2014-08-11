314b350d808de244308353519fcd521102a418b9
Gregory Weaver <gregory.weaver@gmail.com>
2014-08-11 15:15:26 -0700

    Support files.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,14 @@
 #
 # Copyright 2014, Loganov Industries, LLC
 #
+
+# Ruby 
+#default['loganov']['client-workstation']['ruby']['version'] = 
+#default['loganov']['client-workstation']['ruby']['url'] = 
+
+# Sublime Text 2
+#default['loganov']['client-workstation']['sublime_text_2']['url'] = 
+
+# Virtual Box
+#default['loganov']['client-workstation']['ruby']['virtual_box']['url'] = 
+#default['loganov']['client-workstation']['ruby']['virtual_box']['key']['url'] = 
